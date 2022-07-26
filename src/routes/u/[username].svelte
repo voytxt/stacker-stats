625fc3425e34ef3c897ca38a043bcbadfcaf4435
Vojta <65811166+vojta-dev@users.noreply.github.com>
2022-07-26 23:28:03 +0200

    Update packages and add some stats

--- src/routes/u/[username].svelte
+++ src/routes/u/[username].svelte
@@ -1,11 +1,14 @@
-<script context="module">
-  export const load = ({ params: { username } }) => ({ props: { username } });
-</script>
-
 <script>
   export let username;
+  export let tetrio;
+
+  const sprint = tetrio?.records?.['40l']?.record?.endcontext?.finalTime;
+  const blitz = tetrio?.records?.blitz?.record?.endcontext?.score;
 </script>
 
-<h1>{username}</h1>
+<!-- svelte-ignore a11y-img-redundant-alt -->
+<img src="https://tetr.io/user-content/avatars/{tetrio?.user?._id}.jpg?rv={tetrio?.user?.avatar_revision}" alt="Profile picture" />
+<h1>{username.toUpperCase()}</h1>
 
-Hi {username}
+<div>40L: {sprint !== undefined ? Math.floor(sprint) / 1000 : 'ERROR'}</div>
+<div>Blitz: {blitz ?? 'ERROR'}</div>

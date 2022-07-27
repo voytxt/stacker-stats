f6ed3fa99cbf527d8f696a191b95dfb1caa9bf8c
Vojta <65811166+vojta-dev@users.noreply.github.com>
2022-07-27 15:20:31 +0200

    Add loading text

--- src/lib/UsernameInput.svelte
+++ src/lib/UsernameInput.svelte
@@ -2,16 +2,25 @@
   import { goto } from '$app/navigation';
 
   let username = '';
+  let navigating = false;
 
   function handleKeydown(event) {
     if (event.key === 'Enter') {
-      goto(`/u/${username}`);
-      username = '';
+      goto(`/u/${username}`).then(() => {
+        navigating = false;
+        username = '';
+      });
+
+      navigating = true;
     }
   }
 </script>
 
-<input on:keydown={handleKeydown} bind:value={username} type="text" placeholder="Enter TETR.IO or Jstris username" size="30" />
+<div>
+  <input on:keydown={handleKeydown} bind:value={username} type="text" placeholder="Enter TETR.IO or Jstris username" size="30" />
+
+  {#if navigating} <div>Loading...</div> {/if}
+</div>
 
 <style>
   input {
